0c9ee5cffbd2ab4fe8623159198eb63e8409402c
Marissa O'Leary <moleary@mymeliora.com>
2025-03-25 15:37:38 -0400

    Update to remove localhost references

--- stream-hub-prototype/client/src/services/api.js
+++ stream-hub-prototype/client/src/services/api.js
@@ -1,8 +1,8 @@
+// src/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  // Replace with your actual Render domain
-  baseURL: 'https://box-seat-api.onrender.com/api'
+  baseURL: 'https://box-seat-api.onrender.com/api' 
 });
 
 export default api;
